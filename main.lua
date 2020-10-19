226c0791b1ac10981a1997e99f208a85ea8d6294
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-10-19 15:59:16 +0200

    udate

---
+++ main.lua
@@ -0,0 +1,29 @@
+local App = require('weblit-app')
+local Json = require("json")
+
+App.bind({
+    host = "0.0.0.0",
+    port = 8080
+  })
+
+  App.use(require('weblit-auto-headers'))
+  App.use(require('weblit-etag-cache'))
+
+  App.route({
+    method = "GET",
+    path = "/do/:user/:action",
+    domain = "*.myapp.io"
+  }, function (req, res, go)
+    -- Handle route
+  end)
+
+  App.route({
+    method = "PUT",
+    path = "/cubysatastore/save/:store/:key/:data"
+  }, function (req, res, go)
+    local url = saveFile(req.params.username, req.body)
+    res.code = 201
+    res.headers.Location = url
+  end)
+
+  App.start()
